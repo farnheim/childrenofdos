9489bdc0775aeb057f0282ef7e6f17a9b8bc5c1d
Farnheim <farnheim@Farnheims-MBP.beeline>
2018-10-08 14:24:48 +0300

    - Set vars - Preload images - Page setup - Show content

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,23 @@
-function setup() {
-	createCanvas(windowWidth, windowHeight);
+//set global vars
+let images = [];
+let frame = 0;
+
+//preload images
+function preload(){
+  images[0] = loadImage('img/cod0.png');
 }
 
-function draw() {
+//page setup
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	frameRate(15);
+}
 
+//show content
+function draw() {
+	frame = frame + 1;
+  if (frame > 1) {
+  	frame = 0;
+  }
+  image(images[frame], 0, 0, 384, 272);
 }
